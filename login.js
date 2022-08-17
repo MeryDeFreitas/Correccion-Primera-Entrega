2f11c3c4dbde4bffa39543ddc3727b8c5ac4af3e
Mery De Freitas <67561739+MeryDeFreitas@users.noreply.github.com>
2022-08-16 21:53:20 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -5,25 +5,14 @@ let registroSend = document.getElementById('Registro')
 registroSend.addEventListener('Click', registro)
 
 function registro (){
-
+    alert("Hola")
+    console.log("Registro")
     let usuario = document.getElementById("Usuario").value;
     let contrasena = document.getElementById("Contrasena").value;
 
     console.log(usuario)
     console.log(contrasena)
+    window.location="carrito.html"
 }
 
 //Login
-
-let InicioSesion = document.getElementById('InicioSesion')
-
-InicioSesion.addEventListener('Click', login)
-
-function login (){
-
-    let UsuarioGuardado = document.getElementById("UsuarioGuardado").value;
-    let ContraseñaGuardada = document.getElementById("ContraseñaGuardada").value;
-
-    console.log(UsuarioGuardado)
-    console.log(ContraseñaGuardada)
-}
